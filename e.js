90255637c4dbfec937d943563ffd5e61f17c30fe
JahazielHernandezHoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2021-10-18 18:14:56 -0500

    Add files via upload

---
+++ e.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    busco = document.getElementsById("last")
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+}, 3000);
