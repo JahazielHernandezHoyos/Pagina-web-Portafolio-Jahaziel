97f0ea56566ba6739398f78a30c32d6920cea573
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-01-19 21:08:24 -0500

    main

--- e.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-    busco = document.getElementsById("last")
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-}, 3000);
